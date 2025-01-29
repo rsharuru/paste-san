253df05b3a6f0f1e68340eeb20df44b6ae677066
haruru <git@haruru.dev>
2025-01-30 00:41:58 +0900

    change: svg => high quality png

--- paste-san-button.js
+++ paste-san-button.js
@@ -1,7 +1,7 @@
 tinymce.PluginManager.add('paste_raw_html', function (editor, url) {
 	editor.addButton('paste_raw_html', {
 		tooltip: 'Add HTML',
-		image: url + '/paste-san-button-icon.svg',
+		image: url + '/paste-san-button-icon.png',
 		onclick: function () {
 			var raw_html = window.prompt('Please enter HTML', '');
 			if (!raw_html) {
