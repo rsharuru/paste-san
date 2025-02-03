926815f73022e00fb4c11acb2e00cb4fbb236b3a
haruru <git@haruru.dev>
2025-02-03 20:59:32 +0900

    fix: script required for embedding, flick the blank

--- paste-san-button.js
+++ paste-san-button.js
@@ -4,14 +4,9 @@ tinymce.PluginManager.add('paste_raw_html', function (editor, url) {
 		image: url + '/paste-san-button-icon.png',
 		onclick: function () {
 			var raw_html = window.prompt('Please enter HTML', '');
-			if (!raw_html) {
+			if (!raw_html || !raw_html.trim()) {
 				return;
 			}
-			var sanitized_html = raw_html
-				.replace(/<script[\s\S]*?<\/script>/gi, '')
-				.replace(/on\w+="[^"]*"/g, '')
-				.replace(/javascript:/gi, '')
-				.replace(/data:/gi, '')
 			editor.execCommand('mceInsertContent', 0, sanitized_html);
 		}
 	});
